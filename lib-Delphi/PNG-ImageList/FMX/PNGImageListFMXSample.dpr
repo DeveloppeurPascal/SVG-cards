bd0c81eab3813bd2bc70a040a7a34c2ea1f23c20
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-07-13 18:45:01 +0200

    added a Delphi FireMonkey sample to use the image list

---
+++ lib-Delphi/PNG-ImageList/FMX/PNGImageListFMXSample.dpr
@@ -0,0 +1,45 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  SVG Cards
+
+  SVG Files (c) David Bellot
+  Delphi projects (c) Patrick PREMARTIN
+
+  This project and its content is distributed under LGPL license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SVG-cards
+
+  ***************************************************************************
+  File last update : 2025-07-13T18:01:02.000+02:00
+  Signature : 8016d84b4758b9c628eefa4b67841cc1d40b0f92
+  ***************************************************************************
+*)
+
+program PNGImageListFMXSample;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMain in 'fMain.pas' {Form1},
+  udmSVGcards in 'udmSVGcards.pas' {dmSVGcards: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TdmSVGcards, dmSVGcards);
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ lib-Delphi/PNG-ImageList/FMX/fMain.pas
@@ -0,0 +1,96 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  SVG Cards
+
+  SVG Files (c) David Bellot
+  Delphi projects (c) Patrick PREMARTIN
+
+  This project and its content is distributed under LGPL license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SVG-cards
+
+  ***************************************************************************
+  File last update : 2025-07-13T18:33:46.000+02:00
+  Signature : 01ab4483fc0ce4a4a1f91954db8ad7100583973b
+  ***************************************************************************
+*)
+
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.ImgList,
+  udmSVGcards,
+  FMX.Controls.Presentation,
+  FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Glyph1: TGlyph;
+    btnPrev: TButton;
+    btnNext: TButton;
+    procedure btnPrevClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure btnNextClick(Sender: TObject);
+  private
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.btnNextClick(Sender: TObject);
+begin
+  if Glyph1.ImageIndex < dmSVGcards.ImageList.Count - 1 then
+    Glyph1.ImageIndex := Glyph1.ImageIndex + 1
+  else
+    Glyph1.ImageIndex := 0;
+end;
+
+procedure TForm1.btnPrevClick(Sender: TObject);
+begin
+  if Glyph1.ImageIndex > 0 then
+    Glyph1.ImageIndex := Glyph1.ImageIndex - 1
+  else
+    Glyph1.ImageIndex := dmSVGcards.ImageList.Count - 1;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Glyph1.ImageIndex := 0;
+end;
+
+initialization
+
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
+
+end.
