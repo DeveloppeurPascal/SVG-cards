b5f9f8e79f94ce89783690d798beb2c6cae9ef4d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-07-13 18:43:40 +0200

    added a SVG card export as PNG bitmaps in a Delphi FireMonkey TImageList

---
+++ lib-Delphi/PNG-ImageList/FMX/udmSVGcards.pas
@@ -0,0 +1,72 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  SVG Cards
+
+  SVG Files (c) David Bellot
+  Delphi projects (c) Patrick PREMARTIN
+
+  This project and its content is distributed under LGPL license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Project site :
+  https://github.com/DeveloppeurPascal/SVG-cards
+
+  ***************************************************************************
+  File last update : 2025-07-13T17:52:40.000+02:00
+  Signature : ededca36c6d4a409d0650cf844094ee413f3d91f
+  ***************************************************************************
+*)
+
+unit udmSVGcards;
+
+// ****************************************
+// * Images from folder :
+// * C:\Dev\SVG-cards
+// ****************************************
+//
+// This file contains a TDataModule with a
+// TImageList to use in a FireMonkey project.
+//
+// ****************************************
+// File generator : Folder to FMX Image List v1.2
+// Website : https://folder2fmximagelist.olfsoftware.fr/
+// Generation date : 2025-07-13T17:39:11.739Z
+//
+// Don't do any change on this file.
+// They will be erased by next generation !
+// ****************************************
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmSVGcards = class(TDataModule)
+    ImageList: TImageList;
+  private
+  public
+  end;
+
+var
+  dmSVGcards: TdmSVGcards;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+
+end.
